14af7581e36044c6353e83111e8c559ef887a2cc
burt maclin <burt.maclin@fbi.gov>
2024-05-16 16:37:25 -0400

    "storage account"

--- main.tf
+++ main.tf
@@ -20,32 +20,39 @@ data "azurerm_resource_group" "this" {
   name = var.resource_group_name
 }
 
-
-resource "azurerm_container_group" "example" {
-  name                = "example-continst-1313"
-  location            = data.azurerm_resource_group.this.location
-  resource_group_name = data.azurerm_resource_group.this.name
-  ip_address_type     = "Public"
-  dns_name_label      = "aci-label-643de2ec"
-  os_type             = "Linux"
-
-  container {
-    name   = "hello-world"
-    image  = "mcr.microsoft.com/azuredocs/aci-helloworld"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
-  }
-
-  tags = {
-    environment = "development"
-  }
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoraccount"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-output "fqdn_container_group" {
-    value = azurerm_container_group.example.fqdn
-}
+# resource "azurerm_container_group" "example" {
+#   name                = "example-continst-1313"
+#   location            = data.azurerm_resource_group.this.location
+#   resource_group_name = data.azurerm_resource_group.this.name
+#   ip_address_type     = "Public"
+#   dns_name_label      = "aci-label-643de2ec"
+#   os_type             = "Linux"
+
+#   container {
+#     name   = "hello-world"
+#     image  = "mcr.microsoft.com/azuredocs/aci-helloworld"
+#     cpu    = "0.5"
+#     memory = "1.5"
+
+#     ports {
+#       port     = 80
+#       protocol = "TCP"
+#     }
+#   }
+
+#   tags = {
+#     environment = "development"
+#   }
+# }
+
+# output "fqdn_container_group" {
+#     value = azurerm_container_group.example.fqdn
+# }
